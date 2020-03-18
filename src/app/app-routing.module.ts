6c41b6a491b21b2e561b47c662afdbbcc9eddafd
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2020-03-18 16:04:59 +0100

    added page for new list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'lists/:listId', loadChildren: () => import('./todo-list/todo-list.module').then(m => m.TodoListPageModule) }
+  { path: 'lists/:listId', loadChildren: () => import('./todo-list/todo-list.module').then(m => m.TodoListPageModule) },
+  { path: 'add-list', loadChildren: () => import('./add-list/add-list.module').then(m => m.AddListPageModule) }
 ];
 
 @NgModule({
