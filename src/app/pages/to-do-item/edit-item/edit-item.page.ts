7cb4ccd6b56ab5475fe92697b210fdcda4efe06d
Elen Niedermeyer <elen.niedermeyer@posteo.de>
2020-04-10 20:47:29 +0200

    designed edit item page

--- src/app/pages/to-do-item/edit-item/edit-item.page.ts
+++ src/app/pages/to-do-item/edit-item/edit-item.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { ToDoItem } from 'src/app/item';
-import { ItemsService } from 'src/app/services/items.service';
 import { ErrorAlertService } from 'src/app/services/error-alert.service';
+import { ToDoItemsService } from 'src/app/services/to-do-items.service';
 
 @Component({
   selector: 'app-edit-item',
@@ -19,10 +20,11 @@ export class EditItemPage implements OnInit {
   } /*TODO*/
 
   constructor(
-    private itemsService: ItemsService,
+    private itemsService: ToDoItemsService,
     private errorAlertService: ErrorAlertService,
     private route: ActivatedRoute,
-    private router: Router
+    private alertController: AlertController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -39,6 +41,34 @@ export class EditItemPage implements OnInit {
     let res = await this.itemsService.updateItem(this.listDocId, this.item)
     if (res.result) {
       this.router.navigate(['/item', this.listDocId, this.item.docId])
+    } else {
+      // an error appeared
+      this.errorAlertService.showErrorAlert(res.data);
+    }
+  }
+
+  /* TODO: redundant with settings */
+  async deleteItem() {
+    const alert = await this.alertController.create({
+      header: 'Delete Item',
+      message: 'Are you sure you want to delete this item?',
+      buttons: [
+        'No',
+        {
+          text: 'Yes',
+          handler: async () => {
+            this.handleItemDeletion()
+          }
+        }]
+    })
+
+    await alert.present()
+  }
+
+  private async handleItemDeletion() {
+    let res = await this.itemsService.deleteItem(this.listDocId, this.item.docId)
+    if (res.result) {
+      this.router.navigate(['/list', this.listDocId])
     } else {
       // an error appeared
       this.errorAlertService.showErrorAlert(res.data);
